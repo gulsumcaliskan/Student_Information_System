1821857d3a7e4aafcd64109116a16c0a94f7e7f1
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-27 23:52:58 +0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,19 +1,18 @@
-
 public class Course {
 	
-	Teacher teacher; //ref üretildi.
+	Teacher teacher; 
 	String name;
 	String code;
 	String prefix;
 	int note;
-	int sozluNote;
+	int perfNote;
 	
 	Course(String name, String code, String prefix){
 		this.name = name;
 		this.code = code;
 		this.prefix = prefix;
 		int note = 0;
-		int sozluNote = 0;
+		int perfNote = 0;
 
 	}
 	
@@ -21,14 +20,23 @@ public class Course {
 		if (teacher.branch.equals(this.prefix)){
 			this.teacher = teacher;
 			//printTeacher();
+			System.out.println("The process is happened with successfully.");
 		}else {
-			System.out.println("Öðretmen ve ders bölümleri uyuþmuyor.");
+			System.out.println(teacher.name + " can't give this lecture!");
 		}
 		this.teacher = teacher;
 	}
 	
 	void printTeacher(){
 		this.teacher.print();
+		/*
+		if(teacher != null){
+		      System.out.println("The instructor of the "  + this.name + " is: " + teacher.name);
+		}else {
+		      System.out.println("It is not assigned any instructor to the lecture of " + this.name);
+    		}
+		*/
+		
 	}
 
 }
